7d7aae599216bf574bb24be94ce5419abca79891
Erik Sturesson <hej@eriksturesson.se>
2025-05-29 07:59:36 +0200

    Remove dist folder from version control and add to .gitignore

--- dist/index.d.ts
+++
@@ -1,1 +0,0 @@
-export default function errorDrawing(error: any): void;
--- dist/test/test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var __1 = __importDefault(require(".."));
-(0, __1.default)({ code: 500, message: "test" });
